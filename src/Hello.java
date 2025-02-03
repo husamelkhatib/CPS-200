9b11efd811f7d55a6b55eb2648a2519fbbc4345b
husamelkhatib <elkhatib.husam@gmail.com>
2025-02-03 09:54:07 -0600

    forgot to add Hello and Class3Activity in the last commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author:  Husam El-Khatib
+ * date:  1/29/2025
+ * 
+ * Example program that demonstrates println.
+ */
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, Husam!");
+    } // end main
+
+}  // end class Hello
